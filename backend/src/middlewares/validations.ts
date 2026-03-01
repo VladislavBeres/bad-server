3b32bd36a5c14e9bce9de26324f86b3d84761efb
Владислав <vadislavberestyuk@yandex.ru>
2026-03-01 13:41:55 +0300

    проектная работа 'Чиним сайт' версия 1.0.15

--- backend/src/middlewares/validations.ts
+++ backend/src/middlewares/validations.ts
@@ -151,6 +151,6 @@ export const validateSearchQuery = celebrate({
             .optional()
             .default(1)
             .custom((value) => (value === 0 ? 1 : value)),
-        limit: Joi.number().min(1).max(10).optional().default(10),
+        limit: Joi.number().min(0).max(1000).optional().default(10),
     }),
 })
